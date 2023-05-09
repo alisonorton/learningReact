8abdb379bfb942feb9454f353e31c6930cf308bd
Alison Orton <alison.orton@familysearch.org>
2023-05-09 15:24:52 -0600

    App Start React fullstack course

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
+    <BrowserRouter> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Blog</h1>
+      <div id="page-body">
+        <Routes>
+         <Route path="/" element={<HomePage/>}/>
+         <Route path="/about" element={<AboutPage/>}/>
+         <Route path="/article-list" element={<ArticleListPage/>}/>
+         <Route path="/about" element={<AboutPage/>}/>
+         <Route path="/about" element={<AboutPage/>}/>
+
+          
+          
+          <ArticlePage/>
+          <NotFoundPage/>
+        </Routes>
+        
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
